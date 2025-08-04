57296a1ab8f60eaa7dd7d659fa06125458bc05f4
reemaprivate <naikreema535@gmail.com>
2025-08-05 01:42:17 +0530

    added check if add has arguments present

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Calculator do
     calc = Calculator.new
     expect(calc).to respond_to("add")
   end
+
+  it 'adds allows argument' do
+    expect(Calculator.new.method(:add).arity).to be > (0)
+  end
 end
