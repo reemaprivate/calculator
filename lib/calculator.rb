fedc3b795d88b78c33b5146716dd55f8c2ff9f02
reemaprivate <naikreema535@gmail.com>
2025-08-05 12:53:31 +0530

    handle new line delimiter along with comma

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,7 @@ class Calculator
 
   def add numbers
     return "Please pass string as input!" unless numbers.is_a?String
-
-    numbers.split(",").map(&:to_i).sum
+    delimiters = [',', '\n', /\n/]
+    numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
   end
 end
