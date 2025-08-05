a6c6dbe99d03aa4ae9d4ce36f9d6a2c57b7bc733
reemaprivate <naikreema535@gmail.com>
2025-08-05 15:50:42 +0530

    added required note and re-updated code

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,20 +2,16 @@ class Calculator
 
   def add numbers
     return "Please pass string as input!" unless numbers.is_a?String
-    if numbers.start_with?"//"
-      new_delimiter = numbers.split("//")[1][0]
-      delimiters =  [',', '\n', /\n/,new_delimiter]
-    else
-      delimiters =  [',', '\n', /\n/]
-    end
+
+    ##preparing list of delimiters and then sorting the array after converting all elements to integer
+    # when we partition the list, only if all are positive we calculate the sum.
+    
+    delimiters = (numbers.start_with?"//") ? [',', '\n', /\n/,numbers.split("//")[1][0]] : [',', '\n', /\n/]
 
     numbers_list = numbers.split(Regexp.union(delimiters)).map(&:to_i).sort
     return 0 if numbers_list.empty?
     negative_nums = numbers_list.partition{|n| n.negative?}[0]
-    if negative_nums.empty?
-      numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
-    else
-      "negative numbers not allowed #{negative_nums.join(",")}"
-    end
+
+    negative_nums.empty? ?  numbers.split(Regexp.union(delimiters)).map(&:to_i).sum :  "negative numbers not allowed #{negative_nums.join(",")}"
   end
 end
