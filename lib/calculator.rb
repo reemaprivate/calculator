323da68530f7fa5697e48e481e6fa0f480473a8e
reemaprivate <naikreema535@gmail.com>
2025-08-05 13:22:42 +0530

    added support for new delimiters

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -2,7 +2,12 @@ class Calculator
 
   def add numbers
     return "Please pass string as input!" unless numbers.is_a?String
-    delimiters = [',', '\n', /\n/]
+    new_delimiter=''
+    if numbers.start_with?"//"
+      new_delimiter = numbers.split("//")[1][0]
+    end
+
+    delimiters = [',', '\n', /\n/,new_delimiter]
     numbers.split(Regexp.union(delimiters)).map(&:to_i).sum
   end
 end
